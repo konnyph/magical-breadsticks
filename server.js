2c9ecb9844f738dd7bfd1cff67f63dcb23285575
Eugene Park <e.park5336@gmail.com>
2023-02-06 19:17:08 -0500

    have express server loading index page to front

--- server.js
+++ server.js
@@ -8,3 +8,9 @@ let port = process.env.PORT;
 if (port == null || port == "") {
   port = 3001;
 }
+
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => res.sendFile(`${__dirname}/public/index.html`));
