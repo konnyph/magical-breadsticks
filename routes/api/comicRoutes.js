34bba3fb0f20b844395de78df7884510745b3557
Eugene Park <e.park5336@gmail.com>
2023-02-13 18:14:55 -0500

    stashing for pull

---
+++ routes/api/comicRoutes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const user = require('../../models/User');
+const bcrypt = require('bcrypt');
+const sessions = require('express-session');
+const path = require('path')
+
+
+
+
+
+module.exports = router
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const userRoute = require('./userRoutes');
+const comicRoute = require('./comicRoutes');
 
 router.use('/user', userRoute);
+router.use('/comicFetch', comicRoute);
 
 module.exports = router;
